62fbe4389d8d63d63393a7e16a2c9949a09e941f
robiul3b3 <robiul3b3@gmail.com>
2024-02-21 15:22:44 +0600

    feat: created download model schema

---
+++ src/app/modules/download/download.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose'
+import { IDownload, downloadModel } from './download.interface'
+
+const downloadSchema = new Schema<IDownload, downloadModel>(
+  {
+    assetsId: {
+      type: String,
+      required: [true, 'Assets Id Is required'],
+    },
+    userId: {
+        type: String,
+        required: [true, "User Id is required"]
+    },
+    userEmail: {
+        type: String,
+        required: [true, "User Email is required"]
+    }
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+)
+
+export const Keyword = model<IDownload, downloadModel>('download', downloadSchema);
