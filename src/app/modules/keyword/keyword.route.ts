064c432e81848f377b0fd8d678341151be1885ac
robiul3b3 <robiul3b3@gmail.com>
2024-02-22 15:07:37 +0600

    fix: route url of get one keyword missing a params

--- src/app/modules/keyword/keyword.route.ts
+++ src/app/modules/keyword/keyword.route.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.post("/insertKeyword", keywordController.keywordPost);
 router.get("/getKeywords", keywordController.keywordGet);
-router.get("/getOneKeyword", keywordController.oneKeywordGet);
+router.get("/getOneKeyword/:id", keywordController.oneKeywordGet);
 router.get("/getTrendingKeywords", keywordController.trendingKeywordsGet);
 
 export const KeywordRoute = router
