76d28ca6edd35f02d2018f4120c328469408c3fa
mamun2519 <juborajislam46@gmail.com>
2024-06-04 10:28:34 +0600

    route change

--- src/app.ts
+++ src/app.ts
@@ -16,19 +16,19 @@ app.use([
 
 //* application route
 app.use('/api/v1/', ApplicationRootRoute)
-// api test
+//* api test
 app.get('/health', (req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('Server Is Run Successfully')
 })
 
-// root api
+//* root api
 app.get('/', (req: Request, res: Response) => {
   res.status(StatusCodes.OK).send({ success: true, message: 'Server is run' })
 })
 
-// handle global error
+//* handle global error
 app.use(globalErrorHandler)
-// Not Found Route
+//* Not Found Route
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(300).json({
     success: false,
